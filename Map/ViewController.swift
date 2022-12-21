f78c9279e9baaf4e95980817d96b4e3ecf3ec0f5
doomout <winquake3@hanmail.net>
2022-12-22 04:43:57 +0900

    위도와 경도로 원하는 위치 표시, 위치 정보 추출해 텍스트로 표시

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -19,7 +19,20 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation() //위치 업데이트를 시작
         myMap.showsUserLocation = true //위치 보기 값을 true로 설정
     }
-
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        myMap.setRegion(pRegion, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last //위치가 업데이트 되면 먼저 마지막 위치 값을 찾아낸다.
+        //마지막 위치의 위도와 경도 값을 가지고 함수를 호출 한다.
+        //이때 delta 값은 지도의 크기를 정하고, 값이 작을수록 확대된다. 0.01로 설정해쓰니 지도의 100배로 확대해서 보여준다.
+        goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        
+    }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
     }
