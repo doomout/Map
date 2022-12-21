4e13a176717c854540def86151ed0db80b77b27f
doomout <winquake3@hanmail.net>
2022-12-22 04:20:43 +0900

    지도 보여주기 구현

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -1,15 +1,23 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
     @IBOutlet var myMap: MKMapView!
     @IBOutlet var lblLocationInfo1: UILabel!
     @IBOutlet var lblLocationInfo2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lblLocationInfo1.text = ""
+        lblLocationInfo2.text = ""
+        locationManager.delegate = self //델리게이트를 self로 설정
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //정확도를 최고로 설정
+        locationManager.requestWhenInUseAuthorization() //위치 데이터를 추적하기 위해 사용자에게 승인을 요청
+        locationManager.startUpdatingLocation() //위치 업데이트를 시작
+        myMap.showsUserLocation = true //위치 보기 값을 true로 설정
     }
 
 
