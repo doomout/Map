a3d562f5fe8a0ab65a467821112773c75bcf50f6
doomout <winquake3@hanmail.net>
2022-12-22 16:25:21 +0900

    위치 정보 추출하여 텍스트로 표시

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -20,8 +20,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         myMap.showsUserLocation = true //위치 보기 값을 true로 설정
     }
     func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span :Double) {
+        //위도 값과 경도 값을 매개변수로 하여 함수를 호출하고 리턴값을 저장
         let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        
+        //범위 값을 매개변수로 하여 함수를 호출하고 리턴값을 저장
         let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+    
         let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
         myMap.setRegion(pRegion, animated: true)
     }
@@ -31,7 +35,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         //마지막 위치의 위도와 경도 값을 가지고 함수를 호출 한다.
         //이때 delta 값은 지도의 크기를 정하고, 값이 작을수록 확대된다. 0.01로 설정해쓰니 지도의 100배로 확대해서 보여준다.
         goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+        CLGeocoder().reverseGeocodeLocation(pLocation!, completionHandler: {
+            (placemarks, error) -> Void in //장소 표시, 에러를 파라미터로 받으면
+            let pm = placemarks!.first //장소 표시의 첫 부분만 상수에 저장
+            let country = pm!.country //나라 값을 상수에 저장
+            var address:String = country! //지역 값이 있으면 주소 문자열에 추가
+            if (pm!.locality != nil) { //pm상수에서 지역 값이 존재하면..
+                address += " " //빈칸 넣고
+                address += pm!.locality! //문자열에 추가
+            }
+            if (pm!.thoroughfare != nil) { //pm상수에서 도로 값이 존재하면...
+                address += " "//빈칸 넣고
+                address += pm!.thoroughfare! //문자열에 추가
+            }
+            
+            self.lblLocationInfo1.text = "현재 위치"
+            self.lblLocationInfo2.text = address //문자열의 값을 레이블에 표시
+        })
         
+        locationManager.stopUpdatingLocation() //마지막으로 위치 업데이트 되는 것을 정지
     }
 
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
